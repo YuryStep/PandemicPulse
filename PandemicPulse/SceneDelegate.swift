0dc558e85d00a6ee5de4868d7707ec3c7ca262cf
Yury Stepanchuk <stepanchuk.yury@gmail.com>
2024-03-24 12:16:53 +0300

    Add Swiftlint and SwiftFormat to the project

--- PandemicPulse/SceneDelegate.swift
+++ PandemicPulse/SceneDelegate.swift
@@ -8,20 +8,13 @@
 import UIKit
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
     var window: UIWindow?
 
-
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+    func scene(_ scene: UIScene, willConnectTo _: UISceneSession, options _: UIScene.ConnectionOptions) {
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
         let viewController = ViewController()
         window?.rootViewController = viewController
         window?.makeKeyAndVisible()
     }
-
-
-
-
 }
-
